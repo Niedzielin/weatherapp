f2b17784c3ad60b6a09ab549f83911610db68b97
Pjoter <pitersoncs@gmail.com>
2021-01-24 10:29:02 +0100

    Initial User Interface implementation.

---
+++ src/main/java/com/sda/wapp/ConsoleUserInterface/HeadMenuNavigation.java
@@ -0,0 +1,15 @@
+package com.sda.wapp.ConsoleUserInterface;
+
+/**
+ * Created by Hyperbook on 24.01.2021
+ **/
+public class HeadMenuNavigation {
+    static void addNewLocation() {
+    }
+    static void displayUserLocations() {
+    }
+    static void downloadCurrentData(){
+
+    }
+
+}
---
+++ src/main/java/com/sda/wapp/ConsoleUserInterface/UserInterface.java
@@ -0,0 +1,46 @@
+package com.sda.wapp.ConsoleUserInterface;
+
+import java.util.Scanner;
+
+/**
+ * Created by Hyperbook on 24.01.2021
+ **/
+public class UserInterface {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome in our Weather app, Functionalities are on the way.");
+
+        while(true){
+            System.out.println("What would you like to do?");
+            System.out.println("1. Add new location.");
+            System.out.println("2. Display my locations.");
+            System.out.println("3. Download current Weather data.");
+            System.out.println("4. Close app.");
+            int headNavigation=scanner.nextInt();
+            switch (headNavigation) {
+                case 1:
+                    System.out.println();
+                    HeadMenuNavigation.addNewLocation();
+                    break;
+                case 2:
+                    System.out.println();
+                    HeadMenuNavigation.displayUserLocations();
+                    break;
+                case 3:
+                    System.out.println();
+                    HeadMenuNavigation.downloadCurrentData();
+                    break;
+                case 4:
+                    System.out.println();
+                    return;
+            }
+        }
+
+    }
+
+
+
+
+}
