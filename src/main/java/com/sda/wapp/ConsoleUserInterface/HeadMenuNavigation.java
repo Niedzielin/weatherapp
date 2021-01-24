0c2a9f406787323a94246ecb9f3c032f612edb8c
Pjoter <pitersoncs@gmail.com>
2021-01-24 11:11:04 +0100

    User Interface update 0.2

--- src/main/java/com/sda/wapp/ConsoleUserInterface/HeadMenuNavigation.java
+++ src/main/java/com/sda/wapp/ConsoleUserInterface/HeadMenuNavigation.java
@@ -7,19 +7,42 @@ import java.util.Scanner;
  **/
 public class HeadMenuNavigation {
     static Scanner scanner = new Scanner(System.in);
-
+    private static String city, longitude,latitude,region,country;
     static void addNewLocation() {
-        System.out.println("City:");
-        String city = scanner.nextLine();
-        System.out.println("Add geographic coordinates");
-        System.out.println("Longitude");
-        String longitude = scanner.nextLine();
-        System.out.println("Latitude");
-        String latitude = scanner.nextLine();
-        System.out.println("Region");
-        String region = scanner.nextLine();
-        System.out.println("Country");
-        String country = scanner.nextLine();
+        System.out.println("Please insert Location data");
+        System.out.println("1.City");
+        System.out.println("2.Longitude");
+        System.out.println("3.Latitude");
+        System.out.println("4.Region");
+        System.out.println("5.Country");
+        System.out.println("6.Exit");
+        while (true) {
+            int cityNavigation = scanner.nextInt();
+            switch (cityNavigation) {
+                case 1:
+                    System.out.println("City:");
+                    city = scanner.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Longitude");
+                    longitude = scanner.nextLine();
+                    break;
+                case 3:
+                    System.out.println("Latitude");
+                    latitude = scanner.nextLine();
+                break;
+                case 4:
+                    System.out.println("Region");
+                    region = scanner.nextLine();
+                break;
+                case 5:
+                    System.out.println("Country");
+                    country = scanner.nextLine();
+                break;
+                case 6:
+                    return;
+            }
+        }
     }
     static void displayUserLocations() {
         //database merch comeing soon.
